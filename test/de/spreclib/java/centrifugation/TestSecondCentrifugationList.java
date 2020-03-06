1a80b12418b249e41b1eafc2e5111d828034bc03
Christopher Meyer <c.meyer299@web.de>
2020-03-06 16:56:03 +0100

    Adds testSecondCentrifugationListContents toTestSecondCentrifugationList

--- test/de/spreclib/java/centrifugation/TestSecondCentrifugationList.java
+++ test/de/spreclib/java/centrifugation/TestSecondCentrifugationList.java
@@ -2,16 +2,144 @@ package de.spreclib.java.centrifugation;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import de.spreclib.java.enums.SprecPartType;
+import de.spreclib.java.enums.centrifugation.CentrifugationBraking;
+import de.spreclib.java.enums.centrifugation.CentrifugationDuration;
+import de.spreclib.java.enums.centrifugation.CentrifugationSpeed;
+import de.spreclib.java.enums.centrifugation.CentrifugationTemperature;
+import de.spreclib.java.enums.centrifugation.CentrifugationType;
+import de.spreclib.java.spreclib.CodePart;
+import de.spreclib.java.spreclib.centrifugation.Centrifugation;
+import de.spreclib.java.spreclib.centrifugation.NormalCentrifugation;
 import de.spreclib.java.spreclib.centrifugation.SecondCentrifugationList;
+import de.spreclib.java.spreclib.centrifugation.SpecialCentrifugation;
 
 public class TestSecondCentrifugationList {
+
+  Set<Centrifugation> testList;
+
+  @Before
+  public void setUp() {
+    testList = new HashSet<>();
+    testList.add(
+        new SpecialCentrifugation(
+            CentrifugationType.NO, new CodePart("N"), SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new SpecialCentrifugation(
+            CentrifugationType.UNKNOWN, new CodePart("X"), SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new SpecialCentrifugation(
+            CentrifugationType.OTHER, new CodePart("Z"), SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.LESS_THREETHOUSAND_G,
+            CentrifugationTemperature.ROOM_TEMPERATURE,
+            CentrifugationBraking.NO_BRAKING,
+            new CodePart("A"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.LESS_THREETHOUSAND_G,
+            CentrifugationTemperature.ROOM_TEMPERATURE,
+            CentrifugationBraking.NO_BRAKING,
+            new CodePart("B"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.LESS_THREETHOUSAND_G,
+            CentrifugationTemperature.TWO_TO_TEN_DEGREES,
+            CentrifugationBraking.NO_BRAKING,
+            new CodePart("C"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.LESS_THREETHOUSAND_G,
+            CentrifugationTemperature.TWO_TO_TEN_DEGREES,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("D"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.THREETHOUSAND_TO_SIXTHOUSAND_G,
+            CentrifugationTemperature.ROOM_TEMPERATURE,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("E"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.THREETHOUSAND_TO_SIXTHOUSAND_G,
+            CentrifugationTemperature.TWO_TO_TEN_DEGREES,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("F"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.SIXTHOUSAND_TO_TENTHOUSAND_G,
+            CentrifugationTemperature.ROOM_TEMPERATURE,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("G"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.SIXTHOUSAND_TO_TENTHOUSAND_G,
+            CentrifugationTemperature.TWO_TO_TEN_DEGREES,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("F"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.GREATER_TENTHOUSAND_G,
+            CentrifugationTemperature.ROOM_TEMPERATURE,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("I"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+    testList.add(
+        new NormalCentrifugation(
+            CentrifugationType.DEFAULT,
+            CentrifugationDuration.TEN_TO_FIFTEEN_MINUTES,
+            CentrifugationSpeed.GREATER_TENTHOUSAND_G,
+            CentrifugationTemperature.TWO_TO_TEN_DEGREES,
+            CentrifugationBraking.WITH_BRAKING,
+            new CodePart("J"),
+            SprecPartType.SECOND_CENTRIFUGATION));
+  }
 
   @Test
   public void testSecondCentrifugationListSize() {
     int actualSize = SecondCentrifugationList.centrifugations.size();
-    int expectedSize = 13;
+    int expectedSize = testList.size();
     assertEquals(expectedSize, actualSize);
+  }
+
+  @Test
+  public void testSecondCentrifugationListContents() {
+    Object[] actual = SecondCentrifugationList.centrifugations.toArray();
+    Object[] expected = testList.toArray();
+    Assert.assertArrayEquals(expected, actual);
   }
 }
