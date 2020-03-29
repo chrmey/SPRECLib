653acc57c5161ba81dfa5aedb78500e09c186a4d
Christopher Meyer <c.meyer299@web.de>
2020-03-29 15:31:21 +0200

    Changes bound type to Integer
    Changes from int to Integer to pass null value, to realize open bounds

--- src/de/spreclib/model/enums/postcentrifugation/PostCentrifugationDelay.java
+++ src/de/spreclib/model/enums/postcentrifugation/PostCentrifugationDelay.java
@@ -5,9 +5,9 @@ public enum PostCentrifugationDelay {
   ONE_TO_TWO_HOURS(60, 120),
   TWO_TO_EIGHT_HOURS(120, 480),
   EIGHT_TO_TWENTYFOUR_HOURS(480, 1140),
-  GREATER_TWENTYFOUR_HOURS(1140, 999999999);
+  GREATER_TWENTYFOUR_HOURS(1140, null);
 
-  PostCentrifugationDelay(int lowerBound, int upperBound) {
+  PostCentrifugationDelay(Integer lowerBound, Integer upperBound) {
     // TODO Auto-generated constructor stub
     // TODO Bounds überarbeiten
   }
