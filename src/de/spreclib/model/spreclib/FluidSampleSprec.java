6ec14bb2b76d82353c050988d947c52d93a0b12a
Christopher Meyer <c.meyer299@web.de>
2020-03-30 18:27:46 +0200

    Deletes Interfaces in Model

--- src/de/spreclib/model/spreclib/FluidSampleSprec.java
+++ src/de/spreclib/model/spreclib/FluidSampleSprec.java
@@ -1,61 +1,24 @@
 package de.spreclib.model.spreclib;
 
-import de.spreclib.model.enums.SprecPartType;
-import de.spreclib.model.interfaces.IFluidSampleSprec;
-import de.spreclib.model.interfaces.ISprecPart;
-
-public final class FluidSampleSprec extends Sprec implements IFluidSampleSprec {
+public final class FluidSampleSprec extends Sprec {
 
   public FluidSampleSprec() {
     super();
   }
 
-  @Override
+
   public boolean isFluidSample() {
     return true;
   }
 
-  @Override
+ 
   public boolean isSolidSample() {
     return false;
   }
 
-  @Override
-  public ISprecPart getFluidSampleType() {
-    return this.sprecParts.get(SprecPartType.FLUID_SAMPLE_TYPE);
-  }
 
-  @Override
-  public ISprecPart getPrimaryContainer() {
-    return this.sprecParts.get(SprecPartType.PRIMARY_CONTAINER);
-  }
 
-  @Override
-  public ISprecPart getPreCentrifugation() {
-    return this.sprecParts.get(SprecPartType.PRE_CENTRIFUGATION);
-  }
-
-  @Override
-  public ISprecPart getFirstCentrifugation() {
-    return this.sprecParts.get(SprecPartType.FIRST_CENTRIFUGATION);
-  }
-
-  @Override
-  public ISprecPart getSecondCentrifugation() {
-    return this.sprecParts.get(SprecPartType.SECOND_CENTRIFUGATION);
-  }
-
-  @Override
-  public ISprecPart getPostCentrifugation() {
-    return this.sprecParts.get(SprecPartType.POST_CENTRIFUGATION);
-  }
-
-  @Override
-  public ISprecPart getLongTermStorage() {
-    return this.sprecParts.get(SprecPartType.LONG_TERM_STORAGE);
-  }
-
-  @Override
+  
   public String getSprecCode() {
     // TODO Auto-generated method stub
     return null;
--- src/de/spreclib/model/spreclib/Sprec.java
+++ src/de/spreclib/model/spreclib/Sprec.java
@@ -1,21 +1,9 @@
 package de.spreclib.model.spreclib;
 
-import de.spreclib.model.enums.SprecPartType;
-import de.spreclib.model.interfaces.ISprec;
-import de.spreclib.model.interfaces.ISprecPart;
-import java.util.HashMap;
-import java.util.Map;
-
-public abstract class Sprec implements ISprec {
-
-  public Map<SprecPartType, ISprecPart> sprecParts;
+public abstract class Sprec {
 
   public Sprec() {
-    this.sprecParts = new HashMap<>();
+ 
   }
 
-  @Override
-  public ISprecPart getLongTermStorage() {
-    return this.sprecParts.get(SprecPartType.LONG_TERM_STORAGE);
-  }
 }
