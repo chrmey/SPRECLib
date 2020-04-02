db8f0b486be7d876a93a7dfbbccf65159f774ea4
Christopher Meyer <c.meyer299@web.de>
2020-04-02 16:00:05 +0200

    Implements localization for WarmIschemiaTime

--- src/de/spreclib/api/lists/options/WarmIschemiaTimeOption.java
+++ src/de/spreclib/api/lists/options/WarmIschemiaTimeOption.java
@@ -4,7 +4,19 @@ import de.spreclib.model.enums.WarmIschemiaTime;
 
 public final class WarmIschemiaTimeOption extends AbstractListOption {
 
+  private final WarmIschemiaTime warmIschemiaTime;
+
   public WarmIschemiaTimeOption(WarmIschemiaTime warmIschemiaTime) {
-    super(warmIschemiaTime);
+    this.warmIschemiaTime = warmIschemiaTime;
+  }
+
+  @Override
+  public String getStringRepresentation() {
+    return NAMES_DEFAULT.getString(this.warmIschemiaTime.name());
+  }
+
+  @Override
+  public WarmIschemiaTime getContainedObject() {
+    return this.warmIschemiaTime;
   }
 }
