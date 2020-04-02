3a05bf203b479e7b44f5d887617eb2d29af90b44
Christopher Meyer <c.meyer299@web.de>
2020-04-02 15:44:52 +0200

    Implements localization for PrimaryContainer

--- src/de/spreclib/api/lists/options/PrimaryContainerOption.java
+++ src/de/spreclib/api/lists/options/PrimaryContainerOption.java
@@ -4,7 +4,19 @@ import de.spreclib.model.enums.PrimaryContainer;
 
 public final class PrimaryContainerOption extends AbstractListOption {
 
+  private final PrimaryContainer primaryContainer;
+
   public PrimaryContainerOption(PrimaryContainer primaryContainer) {
-    super(primaryContainer);
+    this.primaryContainer = primaryContainer;
+  }
+
+  @Override
+  public String getStringRepresentation() {
+    return NAMES_DEFAULT.getString(this.primaryContainer.name());
+  }
+
+  @Override
+  public PrimaryContainer getContainedObject() {
+    return this.primaryContainer;
   }
 }
