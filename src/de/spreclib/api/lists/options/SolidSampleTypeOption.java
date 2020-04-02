717d8ef3b6a04e7d7616f23da9d406d6f259647f
Christopher Meyer <c.meyer299@web.de>
2020-04-02 15:56:09 +0200

    Implements localization for SolidSampleType

--- src/de/spreclib/api/lists/options/SolidSampleTypeOption.java
+++ src/de/spreclib/api/lists/options/SolidSampleTypeOption.java
@@ -4,7 +4,19 @@ import de.spreclib.model.enums.SolidSampleType;
 
 public final class SolidSampleTypeOption extends AbstractListOption {
 
+  private final SolidSampleType solidSampleType;
+
   public SolidSampleTypeOption(SolidSampleType solidSampleType) {
-    super(solidSampleType);
+    this.solidSampleType = solidSampleType;
+  }
+
+  @Override
+  public String getStringRepresentation() {
+    return NAMES_DEFAULT.getString(this.solidSampleType.name());
+  }
+
+  @Override
+  public SolidSampleType getContainedObject() {
+    return this.solidSampleType;
   }
 }
