73affb331b0ca16fcf58fb23cde0bfbd3344a6ef
Christopher Meyer <c.meyer299@web.de>
2020-04-02 14:12:29 +0200

    Adds localization to ColdIschemiaTime

--- src/de/spreclib/api/lists/options/ColdIschemiaTimeOption.java
+++ src/de/spreclib/api/lists/options/ColdIschemiaTimeOption.java
@@ -4,7 +4,20 @@ import de.spreclib.model.enums.ColdIschemiaTime;
 
 public final class ColdIschemiaTimeOption extends AbstractListOption {
 
+  private final ColdIschemiaTime coldIschemiaTime;
+
   public ColdIschemiaTimeOption(ColdIschemiaTime coldIschemiaTime) {
-    super(coldIschemiaTime);
+    this.coldIschemiaTime = coldIschemiaTime;
+  }
+
+  @Override
+  public String getStringRepresentation() {
+    return NAMES_DEFAULT.getString(this.coldIschemiaTime.name());
+  }
+
+
+  @Override
+  public ColdIschemiaTime getContainedObject() {
+    return this.coldIschemiaTime;
   }
 }
