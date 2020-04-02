8f927bd5fce1d5b548ab674f3325aa5cba9cd602
Christopher Meyer <c.meyer299@web.de>
2020-04-02 14:14:13 +0200

    Adds localization to FixationTime

--- src/de/spreclib/api/lists/options/FixationTimeOption.java
+++ src/de/spreclib/api/lists/options/FixationTimeOption.java
@@ -4,8 +4,19 @@ import de.spreclib.model.enums.FixationTime;
 
 public final class FixationTimeOption extends AbstractListOption {
 
+  private FixationTime fixationTime;
+
   public FixationTimeOption(FixationTime fixationTime) {
-    super(fixationTime);
+    this.fixationTime = fixationTime;
   }
 
+  @Override
+  public String getStringRepresentation() {
+    return NAMES_DEFAULT.getString(this.fixationTime.name());
+  }
+
+  @Override
+  public FixationTime getContainedObject() {
+    return this.fixationTime;
+  }
 }
