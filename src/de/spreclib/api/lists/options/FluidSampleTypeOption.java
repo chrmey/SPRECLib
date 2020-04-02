29e043e07ccc4b873a7c83167aa33ea7f7b94183
Christopher Meyer <c.meyer299@web.de>
2020-04-02 14:14:30 +0200

    Adds localization to FluidSampleType

--- src/de/spreclib/api/lists/options/FluidSampleTypeOption.java
+++ src/de/spreclib/api/lists/options/FluidSampleTypeOption.java
@@ -4,8 +4,19 @@ import de.spreclib.model.enums.FluidSampleType;
 
 public final class FluidSampleTypeOption extends AbstractListOption {
 
+  private final FluidSampleType fluidSampleType;
+
   public FluidSampleTypeOption(FluidSampleType fluidSampleType) {
-    super(fluidSampleType);
+    this.fluidSampleType = fluidSampleType;
   }
-  
+
+  @Override
+  public String getStringRepresentation() {
+    return NAMES_DEFAULT.getString(this.fluidSampleType.name());
+  }
+
+  @Override
+  public FluidSampleType getContainedObject() {
+    return this.fluidSampleType;
+  }
 }
