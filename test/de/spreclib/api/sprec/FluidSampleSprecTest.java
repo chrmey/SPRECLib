674ed61af5cf2bc73d3a49a44348af6d23c3a5a9
Christopher Meyer <c.meyer299@web.de>
2020-04-04 12:48:27 +0200

    Adds tests

---
+++ test/de/spreclib/api/sprec/FluidSampleSprecTest.java
@@ -0,0 +1,17 @@
+package de.spreclib.api.sprec;
+
+import org.junit.Before;
+
+public class FluidSampleSprecTest {
+
+   FluidSampleSprec fluidSampleSprec; 
+    
+  @Before
+  public void setUp() {
+
+    fluidSampleSprec = new FluidSampleSprec();
+    
+    
+    
+  }
+}
