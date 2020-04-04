d6d3240e1bf7f47254a03eea8efc2d115b4e0706
Christopher Meyer <c.meyer299@web.de>
2020-04-04 14:12:55 +0200

    Adds nested builder to FluidSprecCode

--- src/de/spreclib/model/sprec/FluidSample.java
+++ src/de/spreclib/model/sprec/FluidSample.java
@@ -2,7 +2,6 @@ package de.spreclib.model.sprec;
 
 import de.spreclib.model.enums.FluidSampleType;
 import de.spreclib.model.enums.PrimaryContainer;
-import de.spreclib.model.interfaces.ICodePart;
 import de.spreclib.model.spreclib.centrifugation.Centrifugation;
 import de.spreclib.model.spreclib.longtermstorage.LongTermStorage;
 import de.spreclib.model.spreclib.postcentrifugation.PostCentrifugation;
@@ -58,44 +57,16 @@ public final class FluidSample extends Sample {
     this.longTermStorage = longTermStorage;
   }
 
-  public FluidSprecCode buildSprecCode() {
+  public FluidSprecCode getSprecCode() {
 
-    ICodePart fluidSampleTypeCode = null;
-    ICodePart primaryContainerCode = null;
-    ICodePart preCentrifugationCode = null;
-    ICodePart firstCentrifugationCode = null;
-    ICodePart secondCentrifugationCode = null;
-    ICodePart postCentrifugationCode = null;
-    ICodePart longTermStorageCode = null;
-
-    if (this.fluidSampleType != null) {
-      fluidSampleTypeCode = this.fluidSampleType.getCodeFromSprecPart();
-    }
-    if (this.primaryContainer != null) {
-      primaryContainerCode = this.primaryContainer.getCodeFromSprecPart();
-    }
-    if (this.preCentrifugation != null) {
-      preCentrifugationCode = this.preCentrifugation.getCodeFromSprecPart();
-    }
-    if (this.firstCentrifugation != null) {
-      firstCentrifugationCode = this.firstCentrifugation.getCodeFromSprecPart();
-    }
-    if (this.secondCentrifugation != null) {
-      secondCentrifugationCode = this.secondCentrifugation.getCodeFromSprecPart();
-    }
-    if (this.postCentrifugation != null) {
-      postCentrifugationCode = this.postCentrifugation.getCodeFromSprecPart();
-    }
-    if (this.longTermStorage != null) {
-      longTermStorageCode = this.longTermStorage.getCodeFromSprecPart();
-    }
-    return new FluidSprecCode(
-        fluidSampleTypeCode,
-        primaryContainerCode,
-        preCentrifugationCode,
-        firstCentrifugationCode,
-        secondCentrifugationCode,
-        postCentrifugationCode,
-        longTermStorageCode);
+    return new FluidSprecCode.FluidSprecCodeBuilder()
+        .withFluidSampleType(this.fluidSampleType)
+        .withPrimaryContainer(this.primaryContainer)
+        .withPreCentrifugation(this.preCentrifugation)
+        .withFirstCentrifugation(this.firstCentrifugation)
+        .withSecondCentrifugation(this.secondCentrifugation)
+        .withPostCentrifugation(this.postCentrifugation)
+        .withLongTermStorage(this.longTermStorage)
+        .build();
   }
 }
--- src/de/spreclib/model/sprec/FluidSprecCode.java
+++ src/de/spreclib/model/sprec/FluidSprecCode.java
@@ -1,8 +1,15 @@
 package de.spreclib.model.sprec;
 
+import de.spreclib.api.sprec.interfaces.ISprecCode;
+import de.spreclib.model.enums.FluidSampleType;
+import de.spreclib.model.enums.PrimaryContainer;
 import de.spreclib.model.interfaces.ICodePart;
+import de.spreclib.model.spreclib.centrifugation.Centrifugation;
+import de.spreclib.model.spreclib.longtermstorage.LongTermStorage;
+import de.spreclib.model.spreclib.postcentrifugation.PostCentrifugation;
+import de.spreclib.model.spreclib.precentrifugation.PreCentrifugation;
 
-public final class FluidSprecCode {
+public final class FluidSprecCode implements ISprecCode {
 
   private final ICodePart fluidSampleTypeCode;
   private final ICodePart primaryContainerCode;
@@ -12,48 +19,119 @@ public final class FluidSprecCode {
   private final ICodePart postCentrifugationCode;
   private final ICodePart longTermStorageCode;
 
-  protected FluidSprecCode(
-      ICodePart fluidSampleTypeCode,
-      ICodePart primaryContainerCode,
-      ICodePart preCentrifugationCode,
-      ICodePart firstCentrifugationCode,
-      ICodePart secondCentrifugationCode,
-      ICodePart postCentrifugationCode,
-      ICodePart longTermStorageCode) {
-    this.fluidSampleTypeCode = fluidSampleTypeCode;
-    this.primaryContainerCode = primaryContainerCode;
-    this.preCentrifugationCode = preCentrifugationCode;
-    this.firstCentrifugationCode = firstCentrifugationCode;
-    this.secondCentrifugationCode = secondCentrifugationCode;
-    this.postCentrifugationCode = postCentrifugationCode;
-    this.longTermStorageCode = longTermStorageCode;
+  private FluidSprecCode(FluidSprecCodeBuilder builder) {
+    this.fluidSampleTypeCode = builder.fluidSampleTypeCode;
+    this.primaryContainerCode = builder.primaryContainerCode;
+    this.preCentrifugationCode = builder.preCentrifugationCode;
+    this.firstCentrifugationCode = builder.firstCentrifugationCode;
+    this.secondCentrifugationCode = builder.secondCentrifugationCode;
+    this.postCentrifugationCode = builder.postCentrifugationCode;
+    this.longTermStorageCode = builder.longTermStorageCode;
   }
 
-  public ICodePart getFluidSampleTypeCode() {
-    return fluidSampleTypeCode;
+  public String getFluidSampleTypeCode() {
+    return this.fluidSampleTypeCode.getStringRepresentation();
   }
 
-  public ICodePart getPrimaryContainerCode() {
-    return primaryContainerCode;
+  public String getPrimaryContainerCode() {
+    return this.primaryContainerCode.getStringRepresentation();
   }
 
-  public ICodePart getPreCentrifugationCode() {
-    return preCentrifugationCode;
+  public String getPreCentrifugationCode() {
+    return this.preCentrifugationCode.getStringRepresentation();
   }
 
-  public ICodePart getFirstCentrifugationCode() {
-    return firstCentrifugationCode;
+  public String getFirstCentrifugationCode() {
+    return this.firstCentrifugationCode.getStringRepresentation();
   }
 
-  public ICodePart getSecondCentrifugationCode() {
-    return secondCentrifugationCode;
+  public String getSecondCentrifugationCode() {
+    return this.secondCentrifugationCode.getStringRepresentation();
   }
 
-  public ICodePart getPostCentrifugationCode() {
-    return postCentrifugationCode;
+  public String getPostCentrifugationCode() {
+    return this.postCentrifugationCode.getStringRepresentation();
   }
 
-  public ICodePart getLongTermStorageCode() {
-    return longTermStorageCode;
+  public String getLongTermStorageCode() {
+    return this.longTermStorageCode.getStringRepresentation();
+  }
+
+  @Override
+  public String getCodeString() {
+    StringBuilder code = new StringBuilder();
+    code.append(this.fluidSampleTypeCode.getStringRepresentation())
+        .append(this.primaryContainerCode.getStringRepresentation())
+        .append(this.preCentrifugationCode.getStringRepresentation())
+        .append(this.firstCentrifugationCode.getStringRepresentation())
+        .append(this.secondCentrifugationCode.getStringRepresentation())
+        .append(this.postCentrifugationCode.getStringRepresentation())
+        .append(this.longTermStorageCode.getStringRepresentation());
+
+    return code.toString();
+  }
+
+  protected static final class FluidSprecCodeBuilder {
+
+    private ICodePart fluidSampleTypeCode = null;
+    private ICodePart primaryContainerCode = null;
+    private ICodePart preCentrifugationCode = null;
+    private ICodePart firstCentrifugationCode = null;
+    private ICodePart secondCentrifugationCode = null;
+    private ICodePart postCentrifugationCode = null;
+    private ICodePart longTermStorageCode = null;
+
+    public FluidSprecCodeBuilder withFluidSampleType(FluidSampleType fluidSampleType) {
+      if (fluidSampleType != null) {
+        this.fluidSampleTypeCode = fluidSampleType.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCodeBuilder withPrimaryContainer(PrimaryContainer primaryContainer) {
+      if (primaryContainer != null) {
+        this.primaryContainerCode = primaryContainer.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCodeBuilder withPreCentrifugation(PreCentrifugation preCentrifugation) {
+      if (preCentrifugation != null) {
+        this.preCentrifugationCode = preCentrifugation.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCodeBuilder withFirstCentrifugation(Centrifugation firstCentrifugation) {
+      if (firstCentrifugation != null) {
+        this.firstCentrifugationCode = firstCentrifugation.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCodeBuilder withSecondCentrifugation(Centrifugation secondCentrifugation) {
+      if (secondCentrifugation != null) {
+        this.secondCentrifugationCode = secondCentrifugation.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCodeBuilder withPostCentrifugation(PostCentrifugation postCentrifugation) {
+      if (postCentrifugation != null) {
+        this.postCentrifugationCode = postCentrifugation.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCodeBuilder withLongTermStorage(LongTermStorage longTermStorage) {
+      if (longTermStorage != null) {
+        this.longTermStorageCode = longTermStorage.getCodeFromSprecPart();
+      }
+      return this;
+    }
+
+    public FluidSprecCode build() {
+      return new FluidSprecCode(this);
+    }
   }
 }
