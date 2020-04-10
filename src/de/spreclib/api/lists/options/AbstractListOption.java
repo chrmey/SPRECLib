f65e7a84fa7365d68417d08d193e6ecc7aeeb348
Christopher Meyer <c.meyer299@web.de>
2020-04-10 22:28:53 +0200

    Changes static variable name

--- src/de/spreclib/api/lists/options/AbstractListOption.java
+++ src/de/spreclib/api/lists/options/AbstractListOption.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 
 public abstract class AbstractListOption implements IListOption {
 
-  private static final Locale localeEn = new Locale("en");
-  protected static final ResourceBundle NAMES_DEFAULT = ResourceBundle.getBundle("names", localeEn);
+  private static final Locale ENGLISH_US = new Locale("en");
+  protected static final ResourceBundle NAMES_DEFAULT =
+      ResourceBundle.getBundle("names", ENGLISH_US);
 }
